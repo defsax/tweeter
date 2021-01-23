833d36ef5e78422d1dca00f691d88c9a715c1823
Perry Defayette <defayetteperry@gmail.com>
2021-01-23 12:35:20 +0000

    Implement character counter using jquery and js.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  const counter = $("#tweet-text").parent().find($("output.counter"));
+
+  $("#tweet-text").on('input',function() {
+    counter[0].value = 140 - $("#tweet-text").val().length;
+    if (counter[0].value < 0)
+      counter.css({"color": "red"});
+    else
+      counter.css({"color": "black"});
+  });
+
+});
+
